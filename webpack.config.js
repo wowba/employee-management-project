74fa85e6b08daeaf9e13023f4d897ea81e2a5260
wowba <leeyuwk54@gmail.com>
2023-08-18 00:14:17 +0900

    css 세팅 적용

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
